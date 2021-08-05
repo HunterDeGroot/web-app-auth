f653c03f48aee905dafcd5794bbced60651b0de2
hunterdegroot <hunterdegroot@gmail.com>
2021-08-04 21:12:40 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const { clientOrigins, serverPort } = require("./config/env.dev");
-
 const { messagesRouter } = require("./messages/messages.router");
 
 const app = express();
 const apiRouter = express.Router();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(cors({
   origin: '*'
 }));
 
-console.log('for sure updated')
-
-// app.use(express.json());
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
